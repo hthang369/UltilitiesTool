10fa7c52e6e0c59631f48c1eee516d7060921d33
Thang <thangtran369@hotmail.com.vn>
2020-05-03 20:32:00 +0700

    add

--- SQLToolApp/SQLTool/bin/Debug/Scripts/ConfigModule.sql
+++
@@ -1,9 +0,0 @@
-declare @module_name SYSNAME
-declare @maxid int
-set @module_name = '@ModuleName@'
-
-select @maxid = isnull(max(CSCompanyModuleID), 0) from CSCompanyModules
-
-insert into CSCompanyModules select @maxid + 1,STModuleID, STModuleName from STModules where STModuleName like @module_name
-
-select * from CSCompanyModules
--- SQLToolApp/SQLTool/bin/Debug/Scripts/ViewConnectSql.sql
+++
@@ -1,4 +0,0 @@
-select client_net_address,local_net_address,connect_time,program_name,hostname,loginame,
-db_name(dbid),status
-from master.dbo.sysprocesses a
-join  sys.dm_exec_connections b on spid = session_id order by spid
